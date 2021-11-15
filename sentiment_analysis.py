046e7e8cba4adc6ea58a8b00310c3eab2b87b11a
HarryVu176 <harry@moonivia.com>
2021-11-15 09:56:01 -0500

    ok

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,8 @@
-def compute_tweets():
+def compute_tweets(tweets, key):
+    try:
+        tweetsFile = open(tweets, "r")
+        keyFile = open(key, "r")
+    except FileNotFoundError:
+        print('File not found.')
+
+
